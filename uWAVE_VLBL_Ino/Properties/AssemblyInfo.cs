605c518d1e07b22785a36c2f1d4725fb11b54dfb
Aleksandr <aleksandr.v.dikarev@gmail.com>
2019-11-26 10:38:38 +0400

    Create AssemblyInfo.cs

---
+++ uWAVE_VLBL_Ino/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("uWAVE_VLBL_Ino")]
+[assembly: AssemblyDescription("uWAVE underwater acoustic modem based VLBL\npositioning demo application.\n\n" +
+                               "More information about uWAVE\n" +
+                               "English:\nhttps://github.com/ucnl/Docs/tree/master/EN/Modems/uWAVE\n" +
+                               "Russian:\nhttps://github.com/ucnl/Docs/tree/master/RU/Modems/uWAVE\n\n" +
+                               "Support: mailto:support@unavlab.com\n\n" +
+                               "Source code for arduino board:\n" +
+                               "https://github.com/ucnl/uWAVE_Arduino/blob/master/uWAVE_Example_3.ino\n" +
+                               "Source code for this application:\n" +
+                               "https://github.com/ucnl/uWAVE_VLBL_Ino")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("uWAVE_VLBL_Ino")]
+[assembly: AssemblyCopyright("Copyright © Aleksandr Dikarev 2019")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85ea620e-96a6-44ca-9203-ddc1c6349ca2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
